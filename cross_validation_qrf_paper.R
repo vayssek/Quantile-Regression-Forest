f17f154036d0925e4d4f2799d3861554e5ebb0e1
Kevin Vaysse <vayssek@gmail.com>
2016-09-26 15:04:51 +0200

    ADD the calculation of pi intervales
    The calculation of pI interval is completed

--- cross_validation_qrf_paper.R
+++ cross_validation_qrf_paper.R
@@ -97,6 +97,73 @@ me
 mse
 rmse
 
-##Calculation of the pi for all theoric interval
+##Accuracy plot##
+####Accuracy plot####
+result$cond99<-NA
+result$cond95<-NA
+result$cond90<-NA
+result$cond80<-NA
+result$cond70<-NA
+result$cond60<-NA
+result$cond50<-NA
+result$cond40<-NA
+result$cond30<-NA
+result$cond20<-NA
+result$cond10<-NA
+total<-length(result$Actual)
+for(i in 1:total){
+    if(result$Actual[i]<=result$q995[i]&result$Actual[i]>=result$q005[i])
+    {result$cond99[i]<-1}else{result$cond99[i]<-0}
+    if(result$Actual[i]<=result$q975[i]&result$Actual[i]>=result$q025[i])
+    {result$cond95[i]<-1}else{result$cond95[i]<-0}
+    if(result$Actual[i]<=result$q95[i]&result$Actual[i]>=result$q05[i])
+    {result$cond90[i]<-1}else{result$cond90[i]<-0}
+    if(result$Actual[i]<=result$q90[i]&result$Actual[i]>=result$q10[i])
+    {result$cond80[i]<-1}else{result$cond80[i]<-0}
+    if(result$Actual[i]<=result$q85[i]&result$Actual[i]>=result$q15[i])
+    {result$cond70[i]<-1}else{result$cond70[i]<-0}
+    if(result$Actual[i]<=result$q80[i]&result$Actual[i]>=result$q20[i])
+    {result$cond60[i]<-1}else{result$cond60[i]<-0}
+    if(result$Actual[i]<=result$q75[i]&result$Actual[i]>=result$q25[i])
+    {result$cond50[i]<-1}else{result$cond50[i]<-0}
+  if(result$Actual[i]<=result$q70[i]&result$Actual[i]>=result$q30[i])
+  {result$cond40[i]<-1}else{result$cond40[i]<-0}
+  if(result$Actual[i]<=result$q65[i]&result$Actual[i]>=result$q35[i])
+  {result$cond30[i]<-1}else{result$cond30[i]<-0}
+  if(result$Actual[i]<=result$q60[i]&result$Actual[i]>=result$q40[i])
+  {result$cond20[i]<-1}else{result$cond20[i]<-0}
+    if(result$Actual[i]<=result$q55[i]&result$Actual[i]>=result$q45[i])
+    {result$cond10[i]<-1}else{result$cond10[i]<-0}
+}
+  number99<-sum(result$cond99)
+  number95<-sum(result$cond95)
+  number90<-sum(result$cond90)
+  number80<-sum(result$cond80)
+  number70<-sum(result$cond70)
+  number60<-sum(result$cond60)
+  number50<-sum(result$cond50)
+  number40<-sum(result$cond40)
+  number30<-sum(result$cond30)
+  number20<-sum(result$cond20)
+  number10<-sum(result$cond10)
+  pourcent99<-number99/total*100
+  pourcent95<-number95/total*100
+  pourcent90<-number90/total*100
+  pourcent80<-number80/total*100
+  pourcent70<-number70/total*100
+  pourcent60<-number60/total*100
+  pourcent50<-number50/total*100
+  pourcent40<-number40/total*100
+  pourcent30<-number30/total*100
+  pourcent20<-number20/total*100
+  pourcent10<-number10/total*100
+  y<-as.data.frame(cbind(c(99,95,90,80,70,60,50,40,30,20,10),c(pourcent99,pourcent95,pourcent90,pourcent80,pourcent70,pourcent60,pourcent50,pourcent40,pourcent30,pourcent20,pourcent10)))
+  names(y)<-c("PI","Pourcentage")
+  y<-y/100
+  ap<-qplot(PI,Pourcentage,data=y)
+  ap + geom_abline(intercept = 0,colour="red",size=2)+geom_point(colour = "black", size = 3.5) +labs(title="a")+theme_bw()+scale_y_continuous(limits = c(0, 0.99))+scale_x_continuous(limits = c(0, 0.99))+ylab(label = "Proportion within interval")
 
+write.table(y, "D://CD/Projet_DSM/Quantile_Regression_Forest/results/results_dataframe/qrfpi_result_ph.csv", row.names=FALSE)
+write.table(result, "D://CD/Projet_DSM/Quantile_Regression_Forest/results/results_dataframe/qrf_result_ph.csv", row.names=FALSE)
+  
   
